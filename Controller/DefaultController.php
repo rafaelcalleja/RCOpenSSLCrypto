07fef3ba903215a7fa585e089e165b636d60fbee
Rafael Calleja <rafa.calleja@d-noise.net>
2013-07-24 12:26:55 +0200

    init commit

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace RC\OpenSSLCryptoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('RCOpenSSLCryptoBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ RCOpenSSLCryptoBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace RC\OpenSSLCryptoBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RCOpenSSLCryptoBundle extends Bundle
+{
+}
